2c59bccc41998cba854a995f3a769fef8a3755d4
ZaidAliKhann <zaidgbu247@gmail.com>
2026-07-15 00:25:35 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/BHEL-zaid/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
